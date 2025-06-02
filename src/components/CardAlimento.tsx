9d5c795f9233f45ecf22e0bfff1ffadb84e1cdd3
kaique <kai_que15@hotmail.com>
2025-06-02 09:32:32 -0300

    2

--- src/components/CardAlimento.tsx
+++ src/components/CardAlimento.tsx
@@ -1,4 +1,12 @@
-export function CardAlimento({ alimento }: { alimento: any }) {
+interface Alimento {
+  nome: string;
+  calorias: number;
+  proteina: number;
+  carboidrato: number;
+  gordura: number;
+}
+
+export function CardAlimento({ alimento }: { alimento: Alimento }) {
   return (
     <div className="p-4 border rounded shadow bg-white">
       <h2 className="font-bold">{alimento.nome}</h2>
